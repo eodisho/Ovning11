70c253de2c39d6434f2fbd57642d97d06e559d2e
eodisho <43664664+eodisho@users.noreply.github.com>
2021-03-16 18:27:40 +0100

    Add project files.

---
+++ Data/StorageContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Storage.Models;
+
+namespace Storage.Data
+{
+    //StorageContext klass  använder sig av EntityFramework(ORM mappar mellan databasen och vår C# kod).
+    public class StorageContext : DbContext
+    {
+        public StorageContext (DbContextOptions<StorageContext> options)
+            : base(options)
+        {
+        }
+        //property av typen DbSet<Product> Product som vi använder oss av för att
+        //hämta data från databasen(Titta på den genererade koden i ProductsController).
+        public DbSet<Storage.Models.ProductModel> Product { get; set; }
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Storage.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        [StringLength(20, ErrorMessage ="Max 20 Characters")]
+        public string Name { get; set; }
+        public int Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Orderdate { get; set; }
+        [Required]
+        public string Category { get; set; }
+        public string Shelf { get; set; }
+        public int Count { get; set; }
+        public string Discription { get; set; }
+    }
+}
